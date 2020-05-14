780cf6dcb1caedf4f57960639eecac891190cebd
shotaro228 <lo.s225m.xo@icloud.com>
2020-05-14 12:50:14 +0000

    Revert "Revert "set routing""
    This reverts commit 8230c471c769a5d5c9dd27181ac41df5f11ec5be.
    
    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get "/" => "home#top"
+  
+  get "answer/index" => "answers#index"
+  get "answer/new" => "answers#new"
+  post "answer/create" => "answers#create"
+  get "answer/:id" => "answers#show"
+  get "answer/:id/edit" => "answers#edit"
+  post "answer/:id/update" => "answers#update"
+  post "answer/:id/destroy" => "answers#destroy"
+  
+  get "user/index" => "users#index"
+  get "user/signup" => "users#new"
+  post "user/create" => "users#create"
+  get "user/login" => "users#login_form"
+  post "user/login" => "users#login"
+  post "user/logout" => "users#logout"
+  get "user/:id" => "users#show"
+  get "user/:id/edit" => "users#edit"
+  post "user/:id/update" => "users#update"
+  post "user/:id/destroy" => "users#destroy"
+  
+  
+  
 end
