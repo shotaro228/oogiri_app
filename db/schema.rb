5f0e7e603bcc6b541494e436935fa3578349349f
shotaro228 <lo.s225m.xo@icloud.com>
2020-06-10 18:51:26 +0000

    create user_migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200521113507) do
+ActiveRecord::Schema.define(version: 20200610184252) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -27,8 +27,11 @@ ActiveRecord::Schema.define(version: 20200521113507) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "password"
+    t.text     "introduction"
+    t.string   "image_name"
   end
 
 end
